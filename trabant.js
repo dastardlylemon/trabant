f13d6238d5e5cb72fa483b099155d17fa46ec3a8
James Wu <jwu@efunda.com>
2014-12-22 02:25:49 -0800

    removed xml2js

--- trabant.js
+++ trabant.js
@@ -1,20 +1,20 @@
 var fs = require('fs');
-var xml2js = require('xml2js');
 var request = require('request');
+var cheerio = require('cheerio');
 
 // argument parsing
 var args = process.argv.slice(2);
 if (args.indexOf('-s') >= 0 || args.indexOf('--setup') >= 0) {
-  console.log('setup mode');
+  request(
+    'http://www.registrar.ucla.edu/schedule/schedulehome.aspx',
+    function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        $ = cheerio.load(body);
+        console.log($('#ctl00_BodyContentPlaceHolder_SOCmain_lstTermDisp').children().length);
+      }
+    }
+  );
 } else if (args.length > 0) {
   console.log('invalid command');
 }
 
-request(
-  'http://www.registrar.ucla.edu/schedule/schedulehome.aspx',
-  function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-     // console.log(body);
-    }
-  }
-);
