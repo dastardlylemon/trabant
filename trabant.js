f5012ed069f1af492014b0315ec4df10953bc65c
James Wu <jwu@efunda.com>
2014-12-26 17:05:22 -0800

    pretty print subject selection

--- trabant.js
+++ trabant.js
@@ -5,6 +5,9 @@ var request = require('request');
 var table = require('easy-table');
 var cheerio = require('cheerio');
 
+var CONST_REG_URL = 'http://www.registrar.ucla.edu/schedule/schedulehome.aspx';
+var CONST_SEARCH_URL = 'http://www.registrar.ucla.edu/schedule/crsredir.aspx?termsel=%s&subareasel=%s';
+
 var rl = readline.createInterface(process.stdin, process.stdout);
 rl.setPrompt('> ');
 
@@ -24,8 +27,9 @@ var verifyInput = function(pred, callback) {
 }
 
 var setClass = function() {
+  var w_class = {};
   request(
-    'http://www.registrar.ucla.edu/schedule/schedulehome.aspx',
+    CONST_REG_URL,
     function(error, response, body) {
       if (!error && response.statusCode == 200) { // page loaded, good to go
         util.puts('WELCOME TO TRABANT');
@@ -38,16 +42,60 @@ var setClass = function() {
         var terms = t_obj.map(function(i, el) {
           t_codes.push($(this).val());
           return (i + 1) + '. ' + $(this).text();
-        })
+        });
 
         for (var i = 0; i < t_obj.length; i++) {
           util.puts(terms[i]);
         }
 
         verifyInput(isNumber, function(answer) {
-          console.log(answer);
-        });
+          if (answer > t_obj.length) {
+            util.error('Invalid term number');
+          } else {
+            var term = t_codes[answer - 1];
+            var s_obj = $('#ctl00_BodyContentPlaceHolder_SOCmain_lstSubjectArea').children();
+            var s_codes = [];
+            var subjects = s_obj.map(function(i, el) {
+              s_codes.push($(this).val());
+              return (i + 1) + '. ' + $(this).text();
+            });
 
+            // format the data so it's more readable
+            // this can probably be optimized but whatever
+            var t = new table;
+            var s_table = [];
+            for (var i = 0; i < s_obj.length; i += 3) {
+              var row = {};
+              if (subjects[i]) {
+                row[0] = subjects[i];
+              }
+              if (subjects[i + 1]) {
+                row[1] = subjects[i + 1];
+              }
+              if (subjects[i + 2]) {
+                row[2] = subjects[i + 2];
+              }
+              s_table.push(row);
+            }
+
+            s_table.forEach(function(row) {
+              t.cell('1', row[0]);
+              t.cell('2', row[1]);
+              t.cell('3', row[2]);
+              t.newRow();
+            });
+
+            util.puts(t.print());
+
+            verifyInput(isNumber, function(answer) {
+              if (answer > s_obj.length) {
+                util.error('Invalid subject number');
+              } else {
+                console.log(s_codes[answer - 1].split(' ').join('+'));
+              }
+            });
+          }
+        });
       } else {
         util.error('Failed to fetch registrar!');
       }
