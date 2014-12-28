1dfe13d84dda49d95269f1283785e112c477f816
James Wu <jwu@efunda.com>
2014-12-27 16:20:11 -0800

    course selection

--- trabant.js
+++ trabant.js
@@ -6,7 +6,10 @@ var table = require('easy-table');
 var cheerio = require('cheerio');
 
 var CONST_REG_URL = 'http://www.registrar.ucla.edu/schedule/schedulehome.aspx';
-var CONST_SEARCH_URL = 'http://www.registrar.ucla.edu/schedule/crsredir.aspx?termsel=%s&subareasel=%s';
+var CONST_SUBJ_SRCH_URL = 'http://www.registrar.ucla.edu/schedule/crsredir.aspx?termsel=%s&subareasel=%s';
+var CONST_CRS_SRCH_URL = 'http://www.registrar.ucla.edu/schedule/detselect.aspx?termsel=%s&subareasel=%s&idxcrs=%s';
+
+var term, subject, course;
 
 var rl = readline.createInterface(process.stdin, process.stdout);
 rl.setPrompt('> ');
@@ -24,6 +27,41 @@ var verifyInput = function(pred, callback) {
       verifyInput(pred, callback);
     }
   });
+}
+
+var setCourse = function(url) {
+  request(
+    url,
+    function(error, response, body) {
+      if (!error && response.statusCode == 200) { // page loaded, good to go
+        util.puts('Select a course:');
+
+        $ = cheerio.load(body);
+        var c_obj = $('select').children();
+        var c_codes = [];
+        var courses = c_obj.map(function(i, el) {
+          c_codes.push($(this).val().split(' ').join('+'));
+          return (i + 1) + '. ' + $(this).text();
+        });
+
+        for (var i = 0; i < c_obj.length; i++) {
+          util.puts(courses[i]);
+        }
+
+        verifyInput(isNumber, function(answer) {
+          if (answer > c_obj.length) {
+            util.error('Invalid course number');
+          } else {
+            course = c_codes[answer - 1];
+            var search_sec = util.format(CONST_CRS_SRCH_URL, term, subject, course);
+            setSection(search_sec);
+          }
+        });
+      } else {
+        util.error('Failed to fetch courses!');
+      }
+    }
+  );
 }
 
 var setClass = function() {
@@ -52,7 +90,7 @@ var setClass = function() {
           if (answer > t_obj.length) {
             util.error('Invalid term number');
           } else {
-            var term = t_codes[answer - 1];
+            term = t_codes[answer - 1];
             var s_obj = $('#ctl00_BodyContentPlaceHolder_SOCmain_lstSubjectArea').children();
             var s_codes = [];
             var subjects = s_obj.map(function(i, el) {
@@ -85,13 +123,16 @@ var setClass = function() {
               t.newRow();
             });
 
+            util.puts('Select a subject area:');
             util.puts(t.print());
 
             verifyInput(isNumber, function(answer) {
               if (answer > s_obj.length) {
                 util.error('Invalid subject number');
               } else {
-                console.log(s_codes[answer - 1].split(' ').join('+'));
+                subject = s_codes[answer - 1].split(' ').join('+');
+                var search_class = util.format(CONST_SUBJ_SRCH_URL, term, subject);
+                setCourse(search_class);
               }
             });
           }
